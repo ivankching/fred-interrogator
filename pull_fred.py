a687b05857e256a51882101e2248263063600d06
Ivan Ching <ivan1335@gmail.com>
2026-02-04 11:16:09 +0800

    Add logging for FRED API response

--- pull_fred.py
+++ pull_fred.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 import requests as r
 import os
+import logfire
 
 load_dotenv()
+logfire.configure(send_to_logfire=True)
 
 def search_keywords(keywords: str) -> dict | None:
     """
@@ -21,5 +23,6 @@ def search_keywords(keywords: str) -> dict | None:
     keywords = keywords.replace(" ", "+")
     api_key = str(os.getenv("FRED_API_KEY"))
     url = f"https://api.stlouisfed.org/fred/series/search?search_text={keywords}&api_key={api_key}&file_type=json"
-    response = r.get(url)
-    return response.json()
+    response = r.get(url).json()
+    logfire.info(f"Response: {response}")
+    return response
