7d7b999a3942618eb03f44ac15cfa55e5bdbcc5b
Ivan Ching <ivan1335@gmail.com>
2026-02-04 11:17:02 +0800

    Sanitize keywords for API query string

--- search_agent.py
+++ search_agent.py
@@ -5,6 +5,7 @@ from pydantic_ai import Agent, RunContext
 from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.ollama import OllamaProvider
 from typing import List
+from urllib.parse import quote
 
 from pull_fred import search_keywords
 
@@ -14,17 +15,9 @@ logfire.configure()
 logfire.instrument_pydantic_ai()
 
 
-class Series(BaseModel):
-    """Represents a single series from FRED API."""
-    id: str = Field(description="The ID of the series.")
-    title: str = Field(description="The title of the series.")
-
-class SeriesList(BaseModel):
-    """The overall structure representing a list of Series"""
-    results: List[Series] = Field(description="A list of series from FRED API.")
-
 class Keywords(BaseModel):
-    keywords: str = Field(description="The keywords to search for in the FRED API.")
+    """The overall structure representing a list of keywords"""
+    keywords: List[str] = Field(description="The keywords to search for in the FRED API.")
 
 ollama_model = OpenAIChatModel(
     model_name='granite4:7b-a1b-h',
@@ -37,11 +30,11 @@ keyword_agent = Agent(
     system_prompt="""\
 You are an agent that generates keywords from user input.
 Use the user query provided to generate keywords to search for in the FRED API.
-Pick one set of keywords and respond with only the keywords.
+Pick one set of keywords and respond with only the keywords in a format sanitized for inserting into a HTTP request query parameter. Do not include any other text.
 """
 )
 
-def search_series(keywords: str) -> str:
+async def search_series(keywords: str) -> str:
     """
     This function takes a set of keywords and uses them to search the FRED API.
     It then takes the results from the FRED API and formats them into markdown text.
@@ -54,14 +47,44 @@ def search_series(keywords: str) -> str:
     Returns:
         str: A numbered list of the series in markdown format, with each item including the title and id of the series found.
     """
-    logfire.info(f"Keywords: {keywords}")
+    logfire.info(f"Query String: {keywords}")
     json_response = search_keywords(keywords)
     if json_response:
         seriess = json_response["seriess"]
         md_output = ""
         for i in range(len(seriess)):
-            item = f"{i}. title: {seriess[i]["title"]}\n\t- id: {seriess[i]['id']}\n"
+            item = f"{i+1}. title: {seriess[i]["title"]}\n\t- id: {seriess[i]['id']}\n"
             md_output += item
         logfire.info(f"Tool Results: {md_output}")
         return md_output
     return "No results found"
+
+def sanitize_keywords(keywords: List[str]) -> str:
+    output = []
+    for keyword in keywords:
+        output.append(keyword.replace(" ", "+"))
+    output = "|".join(output)
+    return quote(output, safe="+|")
+
+async def get_seriess_from_query(query: str) -> str:
+    """
+    This function takes a user query and uses it to search the FRED API.
+    It takes the query and uses it to generate keywords, which are then used to search the FRED API.
+    The results from the FRED API are then formatted into markdown text.
+    The markdown text is a numbered list of the series found, with each item including the title and id of the series found.
+    If no results are found, it returns "No results found"
+
+    Args:
+        query (str): The user query to search for in the FRED API.
+
+    Returns:
+        str: A numbered list of the series in markdown format, with each item including the title and id of the series found.
+    """
+    logfire.info(f"Query: {query}")
+    result = await keyword_agent.run(query)
+    logfire.info(f"Keywords: {result.output.keywords}")
+    keywords = sanitize_keywords(result.output.keywords)
+    seriess_md = await search_series(keywords)
+    with open("md_output/seriess.md", "w") as f:
+        f.write(seriess_md)
+    return seriess_md
--- tests/test_search_agent.py
+++ tests/test_search_agent.py
@@ -1,6 +1,7 @@
 import pytest
-from search_agent import search_series, keyword_agent
+from search_agent import search_series, keyword_agent, get_seriess_from_query, sanitize_keywords
 import logfire
+from pathlib import Path
 
 logfire.configure(send_to_logfire=True)
 
@@ -9,8 +10,30 @@ def test_search_series():
     result = search_series(keywords)
     assert result
 
+
 @pytest.mark.asyncio
 async def test_keyword_agent():
     query = "What is the monthly monetary service index?"
     result = await keyword_agent.run(query)
     assert result
+
+def test_sanitize_keywords():
+    keywords = ["Monetary Service Index (MSI)", "Monthly"]
+    result = sanitize_keywords(keywords)
+    expected_result = "Monetary+Service+Index+%28MSI%29|Monthly"
+    assert result == expected_result
+
+@pytest.mark.asyncio
+async def test_search_series_multiple():
+    keywords = ["Monetary Service Index (MSI)", "Monthly"]
+    result = sanitize_keywords(keywords)
+    result = await search_series(result)
+    assert result
+
+@pytest.mark.asyncio
+async def test_get_seriess_from_query():
+    query = "What is the monthly monetary service index?"
+    result = await get_seriess_from_query(query)
+    expected_md_out = Path("md_output/seriess.md")
+    assert result
+    assert expected_md_out.is_file()
