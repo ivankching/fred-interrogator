43f3b70218a561fb88bf463e5a961f1c7dfc3f35
Ivan Ching <ivan1335@gmail.com>
2026-02-05 11:05:57 +0800

    Pull observations from FRED and save zip file

--- pull_fred.py
+++ pull_fred.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import requests as r
 import os
 import logfire
+import zipfile
 
 load_dotenv()
 logfire.configure(send_to_logfire=True)
@@ -26,3 +27,38 @@ def search_keywords(keywords: str) -> dict | None:
     response = r.get(url).json()
     logfire.info(f"Response: {response}")
     return response
+
+def pull_observations(series_id: str) -> dict:
+    """
+    Pull observations from the Federal Reserve Economic Data (FRED) API
+
+    Parameters
+    ----------
+    series_id : str
+        Series ID to pull observations for
+
+    Returns
+    -------
+    dict | None
+        sucess or failure of downloading and saving data, with path to zip file.
+    """
+
+    api_key = str(os.getenv("FRED_API_KEY"))
+    url = f"https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=csv"
+    response = r.get(url)
+    zip_path = f"data/{series_id}.zip"
+    try:
+        with open(zip_path, "wb") as f:
+            f.write(response.content)
+        logfire.info(f"Zip file saved: {zip_path}")
+    except Exception as e:
+        logfire.error(f"Error saving zip file: {e}")
+        return {"success": False, "error": e}
+    # try:
+    #     with zipfile.ZipFile(zip_path, "r") as zip_ref:
+    #         zip_ref.extract("*.csv", "data")
+    #     logfire.info(f"CSV file extracted: {zip_path}")
+    # except Exception as e:
+    #     logfire.error(f"Error extracting CSV file: {e}")
+    #     return {"success": False, "error": e}
+    return {"success": True, "zip_path": zip_path}
--- tests/test_pull_fred.py
+++ tests/test_pull_fred.py
@@ -1,4 +1,4 @@
-from pull_fred import search_keywords
+from pull_fred import search_keywords, pull_observations
 
 def test_search_keywords():
     """Test that the search_keywords function returns a dict with no error code"""
@@ -7,3 +7,10 @@ def test_search_keywords():
     assert result
     assert type(result) == dict
     assert "error_code" not in result
+
+def test_pull_observations():
+    """Test that the pull_observations function returns a dict with no error code"""
+    series_id = "MSIM2"
+    result = pull_observations(series_id)
+    assert result
+    assert result["success"]
